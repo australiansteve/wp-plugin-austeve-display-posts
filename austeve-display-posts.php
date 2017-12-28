cafbaa1dc877008f444b4b5fa8da66321b78bc59
Steve <steve@australiansteve.com>
2017-12-27 21:36:37 -0400

    More options and better handling/flexibility

--- austeve-display-posts.php
+++ austeve-display-posts.php
@@ -37,25 +37,45 @@ class AUSteve_Display_Posts {
 	{
 	    $atts = shortcode_atts( array(
 	    	'post_type' => 'post',
-	        'category' => '',
+	        'category_name' => '',
+	        'tax_name' => '',
+	        'tax_term' => '',
+	        'tax_field' => 'slug',
 	        'format' => 'single',
-	        'posts_per_page' => 5
+	        'posts_per_page' => 5,
+	        'id' => '',
+	        'show_pagination' => 'false',
+	        'container_class' => '',
+	        'post_class' => '',
 	    ), $atts );
 	    
 	    extract( $atts );
 
 	    $args = array(
 	        'post_type' => $post_type,
+	        'p' => $id,
 	        'post_status' => array('publish'),
 	        'posts_per_page' => $posts_per_page,
 	        'paged'	=> get_query_var('paged') ? get_query_var('paged') : 1,
 	    );
 
-	    if ($category != '' && $post_type == 'post')
+
+	    if (!empty($category_name) && $post_type == 'post')
 	    {
 	    	//Only good for post categories
-	    	$args['category_name'] = $category;
+	    	$args['category_name'] = $category_name;	
 	    }
+
+	    if (!empty($tax_name) && !empty($tax_term)) 
+		{
+			$args['tax_query'] = array(
+		        array(
+		            'taxonomy' => $tax_name,
+		            'field' => $tax_field,
+		            'terms' => $tax_term,
+		        )
+		    );
+		}	
 
 	    error_log("Display posts query: ". print_r($args, true));
 	    query_posts( $args );
@@ -65,29 +85,45 @@ class AUSteve_Display_Posts {
 		
 		ob_start();
 	    if ( have_posts() ):
-	    	?>
-		    <div id='<?php echo $post_type; ?>' class='austeve-display-posts'>
-		    <?php
+
+			if (!empty($container_class))
+				echo "<div class='".$container_class."'>";;
+	    	
 		    while ( have_posts() ) :
 		        the_post();
-?>
-				<?php get_template_part( 'components/content', $format ); ?>
-<?php
+
+			    echo "<div id='".$post_type."-".get_the_id()."' class='austeve-display-posts ".$post_class."'>";
+
+				if ($post_type == 'page') :
+		    		the_content();
+		    	else:
+					get_template_part( 'template-parts/content', $format ); 
+				endif;
+				
+				echo "</div>";
+
 		    endwhile;
-		    ?>
-			    <!-- pagination -->
-			    <div class='row pagination'>
-				    <div class='small-6 columns page-link newer-posts'>
-						<?php previous_posts_link( "<< Newer posts" ); ?>
+
+	    	if (!empty($container_class))
+	    		echo "</div>";
+
+		    if ($show_pagination == 'true'):
+			    ?>
+				    <!-- pagination -->
+				    <div class='row pagination'>
+					    <div class='small-6 columns page-link newer-posts'>
+							<?php previous_posts_link( "<< Newer posts" ); ?>
+					    </div>
+					    <div class='small-6 columns text-right page-link older-posts'>
+							<?php next_posts_link( "Older posts >>"); ?>
+					    </div>
 				    </div>
-				    <div class='small-6 columns text-right page-link older-posts'>
-						<?php next_posts_link( "Older posts >>"); ?>
-				    </div>
-			    </div>
-		    </div>
-		    <?php
+			    <?php
+			endif;
+
+	    	
 		else:
-			    	echo "<div id='faqs'>No posts found</div>";
+	    	echo "<div id='faqs'>No posts found</div>";
 		endif;
 		wp_reset_query();
 
